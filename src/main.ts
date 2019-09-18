8319672c89c5ad63f5e053d7925667b5f21c45f3
Brian Schulte <brianr.schulte@gmail.com>
2019-09-18 12:21:33 -0400

    Update with .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.port || 5555);
 }
+
 bootstrap();
